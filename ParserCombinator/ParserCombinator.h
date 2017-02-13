d5f1a744be4e2fa2a862578461d4c12c6e54b304
Liam <l.flynn2@live.co.uk>
2017-02-13 20:42:36 +0000

    Initial Commit

---
+++ ParserCombinator/ParserCombinator.h
@@ -0,0 +1,19 @@
+//
+//  ParserCombinator.h
+//  ParserCombinator
+//
+//  Created by Liam on 13/02/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParserCombinator.
+FOUNDATION_EXPORT double ParserCombinatorVersionNumber;
+
+//! Project version string for ParserCombinator.
+FOUNDATION_EXPORT const unsigned char ParserCombinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParserCombinator/PublicHeader.h>
+
+
