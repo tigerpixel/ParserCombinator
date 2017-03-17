0df2e631f2156cf482491bc154bb03fd1126c6b2
Liam <l.flynn2@live.co.uk>
2017-03-17 10:03:43 +0000

    Delete unsed files from adding targets.

---
+++ ParserCombinator-tvOS/ParserCombinator-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  ParserCombinator-tvOS.h
+//  ParserCombinator-tvOS
+//
+//  Created by Liam on 17/03/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParserCombinator-tvOS.
+FOUNDATION_EXPORT double ParserCombinator_tvOSVersionNumber;
+
+//! Project version string for ParserCombinator-tvOS.
+FOUNDATION_EXPORT const unsigned char ParserCombinator_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParserCombinator_tvOS/PublicHeader.h>
+
+
